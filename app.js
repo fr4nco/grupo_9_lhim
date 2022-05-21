4eede922a09179acf56e907f0965613e88af8955
Franco <francolopezpintos@gmail.com>
2022-05-20 23:06:09 -0300

    se agrega archivos html index y register. Con estilos e imágenes

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ app.listen(3030, () =>
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"))
+});
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+});
