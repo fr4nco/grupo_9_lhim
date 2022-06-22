fb7cb0c56f838e3fd97b6dd59d2ad3d72d5c409c
Juan Ignacio Ingelmo <juaniingelmo@MacBook-Air-de-Juan-3.local>
2022-06-21 23:18:01 -0300

    controllers nuevo

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,14 +3,40 @@ const router = express.Router();
 const productsControllers = require( "../controllers/productsControllers" )
 
 
-router.get("/", productsControllers.index)
+//router.get("/", productsControllers.index)
 
-router.get("/productCart", productsControllers.productCart)
+//router.get("/productCart", productsControllers.productCart)
 
-router.get("/productDetail", productsControllers.productDetail)
+//router.get("/productDetail", productsControllers.productDetail)
 
-router.get("/add", productsControllers.productAdd)
+//router.get("/add", productsControllers.productAdd)
 
-router.get("/edit", productsControllers.productsEdit)
+//router.get("/edit", productsControllers.productsEdit)
 
-module.exports = router
+//module.exports = router
+
+// **** Controller Require ****
+//const productsController = require('../controllers/productsController');
+
+/* GET ALL PRODUCTS */ 
+router.get('/', productsController.index); 
+
+/* CREATE ONE PRODUCT */ 
+router.get('/add', productsController.create); 
+router.post('/store', productsController.store); 
+
+/* GET ONE PRODUCT */ 
+router.get('/detail/:id', productsController.detail); 
+
+/* GET ONE CATEGORY */ 
+router.get('/category/:name', productsController.category); 
+
+/* EDIT ONE PRODUCT */ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/update/:id', productsController.update); 
+
+/* DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', productsController.destroy);
+
+
+module.exports = router;
