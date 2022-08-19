ebcddf5bdfd3b2115873b6a2bd6f553d90ff9430
Juan Ignacio Ingelmo <juaniingelmo@MacBook-Air-de-Juan-3.local>
2022-08-18 23:18:11 -0300

    limpiandocodigo

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,22 +6,7 @@ const usersControllers = require( "../controllers/usersControllers.js");
 const authMiddleware = require( "../middlewares/users/authMiddleware.js");
 const guestMiddleware = require( "../middlewares/users/guestMiddleware.js");
 const upload= require("../middlewares/users/usersmd.js");
-/*
-const diskStorage = multer.diskStorage (
-    {
-        destination: (req, file, cb) => {
-	let folder = '../grupo_9_lhim/public/images/users'
-	cb (null, folder)
 
-},
-	filename: (req, file, cb) => {
-		let avatarName = "avatar" + Date.now() + path.extname(file.originalname)
-    	cb(null, avatarName)
-	}
-})
-;
-*/
-//const upload = multer ({storage: diskStorage});
 
 router.get("/register", guestMiddleware, usersControllers.register);
 
