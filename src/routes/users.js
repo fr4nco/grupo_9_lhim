7fee17699d07d83e85232c2bba3d49c98ab06113
Juan Ignacio Ingelmo <juaniingelmo@192.168.1.2>
2022-08-22 12:38:07 -0300

    validacion de registro solo en rutas, falta controlador

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,10 +7,24 @@ const authMiddleware = require( "../middlewares/users/authMiddleware.js");
 const guestMiddleware = require( "../middlewares/users/guestMiddleware.js");
 const upload= require("../middlewares/users/usersmd.js");
 
+//Validacion de registro de back/end//
+const {body} = require('express-validator');
+const validateRegisterForm = [
+    body('firstname').notEmpty().withMessage('Debes completar tu nombre').bail(),
+    body('firstname').isLength({min:2}).withMessage('Tu nombre debe tener al menos 2 letras'),
+    body('lastname').notEmpty().withMessage('Debes completar tu apellido').bail(),
+    body('lastname').isLength({min:2}).withMessage('Tu apellido debe tener al menos 2 letras'),
+    body('email').notEmpty().withMessage('Debes completar tu mail').bail(),
+    body('email').isEmail().withMessage('Debes completar un emal válido').bail(),
+    //body('email').no debe estar repetido...//
+    body('contrasena').notEmpty().withMessage('Debes completar una contraseña').bail(),
+    body('contrasena').isLength({min:8}).withMessage('Tu contraseña de tener al menos 8 caracteres'),
+    //falta validación de la imagen//
+]
 
 router.get("/register", guestMiddleware, usersControllers.register);
 
-router.post( "/register", upload.single( "avatar" ), usersControllers.createNewUser);
+router.post( "/register", upload.single( "avatar" ), validateRegisterForm, usersControllers.createNewUser);
 
 router.get("/login",guestMiddleware, usersControllers.login);
 
