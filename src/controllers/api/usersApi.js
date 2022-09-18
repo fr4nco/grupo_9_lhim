d94527f57b02da462f2b5e4edd848f3dd5ba024c
Juan Ignacio Ingelmo <juaniingelmo@192.168.1.3>
2022-09-18 18:02:05 -0300

    Fede y Juan, usuarios con el total

--- src/controllers/api/usersApi.js
+++ src/controllers/api/usersApi.js
@@ -2,21 +2,21 @@ const { off } = require('process');
 const bcrypt = require("bcryptjs");
 const db = require('../../database/models/index')
 const {op} = require('sequelize');
+const { rmSync } = require('fs');
 
 
 const controller = {
 
     users: (req, res) => {
-
-
-        db.usuario.findAll(
-			{
-				include: ["rol"]
-			}
-		).then(usuarios => {
-					res.json(usuarios)
-				});
-
+		
+		db.usuario.findAll()
+		.then(usuarios => {
+			return res.status(200).json({
+				total: usuarios.length,
+				data: usuarios,
+				status: 200
+			})
+		})
     },
 
 	userSelected: (req, res) => {
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": '',
+    "password": 'Emrprax2435',
     "database": "bdlhim",
     "host": "127.0.0.1",
     "dialect": "mysql",
